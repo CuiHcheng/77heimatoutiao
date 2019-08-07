22e615f011941e2edc646e49f113c0a59caef5e8
cheng <295025257@qq.com>
2019-08-07 17:01:30 +0800

    导入element-ui和vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div id="app">App</div>
+  <div id="app">
+    <el-switch v-model="value2" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      value1: true,
+      value2: true
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import router from '@/router'
 import App from './App.vue'
 
 // Tip提示信息
 // 开发环境在控制台的提示信息  更详细
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 // main.js 的职责
 // 1.导入项目所需要依赖的资源（js包 css文件 其他资源）
@@ -11,5 +18,6 @@ Vue.config.productionTip = false
 
 // 根实例
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
